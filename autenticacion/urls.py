0ebf283337661329b69bc5e3afcd852739fde8ec
JuanDiegoG <jdiegogarcia0110@gmail.com>
2024-06-28 15:04:57 -0500

    first commit

---
+++ autenticacion/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import VRegistro, cerrar_session, loguear
+
+
+urlpatterns = [
+    path('', VRegistro.as_view(), name='Autenticacion'),
+    path('cerrar_session', cerrar_session, name='cerrar_session'),  
+    path('loguear', loguear, name='loguear'),       
+]
+
